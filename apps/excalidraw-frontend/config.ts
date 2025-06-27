48c3835a35582a1aaadc90aa40a942ea21b1a21d
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-06-28 01:03:32 +0530

    Added prisma for ws-backend for render

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,3 +1,2 @@
-export const HTTP_BACKEND = "http://localhost:3001";
+export const HTTP_BACKEND = "https://excalidraw-2-00rd.onrender.com";
 export const WS_URl = "ws://localhost:8080";
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjYzJlZWNlZi0yYzY0LTQ3NDktOTcxNy0yNzMyYWJiOGFkMDMiLCJpYXQiOjE3NDk2Mjc2NTB9.a2FUKFMUsleH4GFlHECseOEcyqS6O8rnKO84lesv_ho";
