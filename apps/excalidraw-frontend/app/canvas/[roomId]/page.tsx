06039222df917f3390d5a33a21a6ddd00effe129
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-06-28 20:18:42 +0530

    Add the promise to the params in canvas/page.tsx

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -3,14 +3,19 @@
 import RoomCanvas from "@/components/RoomCanvas";
 
 
-export default async function CanvasPage({params}: {
-    params: {
-        roomId: string
-    }
-}){
-    const roomId = (await params).roomId; 
-    console.log(roomId);
+// export default async function CanvasPage({params}: {
+//     params: {
+//         roomId: string
+//     }
+// }){
+//     const roomId = (await params).roomId; 
+//     console.log(roomId);
 
-   return <RoomCanvas roomId={roomId}/>
+//    return <RoomCanvas roomId={roomId}/>
     
+// }
+export default async function CanvasPage({ params }: { params: Promise<{ roomId: string }> }) {
+  const { roomId } = await params;
+
+  return <RoomCanvas roomId={roomId} />;
 }
