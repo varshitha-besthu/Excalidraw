6b345ae167423d0ab3fcede011c942af4685c45b
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-06-28 18:45:46 +0530

    Added the file index.d.ts to handle the req.userId

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 const {JWT_SECRET } = require("@repo/be-common/src/index");
-
+interface JwtPayload {
+  userId: string;
+  // Add more fields if your token includes them
+}
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
@@ -9,7 +12,7 @@ export function middleware(req: Request, res: Response, next: NextFunction) {
     const decoded = jwt.verify(token, JWT_SECRET);
 
     if (decoded) {
-        // @ts-ignore: TODO: Fix this
+        const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
         req.userId = decoded.userId;
         next();
     } else {
